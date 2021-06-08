0ef49f152b127a9e71bd3d53a5539dfac57438f6
Francisco Souza <osf.fjose@creditas.com>
2021-06-07 21:15:26 -0300

    feat: add themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:epub_reader/pages/chapter.dart';
 import 'package:epub_reader/pages/chapters.dart';
+import 'package:epub_reader/themes/dark.dart';
+import 'package:epub_reader/themes/light.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 
 void main() => runApp(
       GetMaterialApp(
+        title: "Epub Reader",
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        themeMode: ThemeMode.system,
         initialRoute: '/chapters',
         getPages: [
           GetPage(
